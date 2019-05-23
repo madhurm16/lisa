afbe473b7bdc0071878e117a37f2ce429dda08a4
Fabien Petit <petit.fabien@outlook.com>
2019-05-23 19:42:59 +0200

    United Nations Data

---
+++ data/raw/un/README.md
@@ -0,0 +1,9 @@
+# United Nations : World Population Prospect
+
+Data downloaded from : https://population.un.org/wpp/Download/Standard/CSV/
+
+- WPP2017_PopulationByAgeSex_Medium.csv : Population by 5-year age groups, annually from 1950 to 2100. 
+Note: for years below 1990, last age available is 80+ (80 and over); for 1990 and over, age groups are available until 100+ (100 and over).
+PopMale: Male population in the age group (thousands)
+PopFemale: Female population in the age group (thousands)
+PopTotal: Total population in the age group (thousands)
--- script/data_initialization.R
+++ script/data_initialization.R
@@ -3,6 +3,21 @@ require(haven)
 
 loc_data = file.path(getwd(), "data")
 
+# CSV DATA to CSV FORMAT : Function
+csv_to_csv = function(loc_raw, loc_csv){
+  
+  files = list.files(loc_raw, pattern = "csv")
+  files_split = strsplit(files, "\\.")
+  
+  for(i in c(1:length(files))){
+    if(files_split[[i]][2] == "csv"){
+      write.csv(read.csv(file.path(loc_raw, files[i])),
+                file.path(loc_csv, paste0(files_split[[i]][1], ".csv")),
+                row.names = FALSE)
+    }
+  }
+}
+
 ##### PENN WORLD TABLE #####
 
 # PWT DATA to CSV FORMAT : Function
@@ -42,25 +57,20 @@ pwt_to_csv(loc_raw_pwt, loc_csv_pwt)
 
 ##### OECD #####
 
-# OECD DATA to CSV FORMAT : Function
-oecd_to_csv = function(loc_raw, loc_csv){
-  
-  files = list.files(loc_raw, pattern = "csv")
-  files_split = strsplit(files, "\\.")
-  
-  for(i in c(1:length(files))){
-    if(files_split[[i]][2] == "csv"){
-      write.csv(read.csv(file.path(loc_raw, files[i])),
-                file.path(loc_csv, paste0(files_split[[i]][1], ".csv")),
-                row.names = FALSE)
-    }
-  }
-}
-
 # Raw data location
 loc_raw_oecd = file.path(loc_data, "raw", "oecd")
 # Csv data location
 loc_csv_oecd = file.path(loc_data, "csv", "oecd")
 
 # Do
-oecd_to_csv(loc_raw_oecd, loc_csv_oecd)
+csv_to_csv(loc_raw_oecd, loc_csv_oecd)
+
+##### UNITED NATIONS : World Population Prospect #####
+
+# Raw data location
+loc_raw_un = file.path(loc_data, "raw", "un")
+# Csv data location
+loc_csv_un = file.path(loc_data, "csv", "un")
+
+# Do
+csv_to_csv(loc_raw_un, loc_csv_un)
