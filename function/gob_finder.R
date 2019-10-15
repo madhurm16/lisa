094b78d455c0c8938d3addf407f8a8995ee779b4
SummerTold <fabien.petit@univ-amu.fr>
2019-10-15 09:50:28 +0200

    gamma Update

--- function/gob_finder.R
+++ function/gob_finder.R
@@ -1,13 +1,13 @@
-gob_finder = function(data, param, gamma_specification = ".cst"){
+gob_finder = function(data, param, gamma_spe = ".cst"){
   # Select omega and beta associated
   param[,"gamma"] = param.gamma %>%
-    select(gamma = paste0("gamma", gamma_specification))
+    select(gamma = paste0("gamma", gamma_spe))
   param[,c("omega", "beta")] =  param.pref %>%
-    select(omega = paste0("omega", gamma_specification),
-           beta = paste0("beta", gamma_specification))
+    select(omega = paste0("omega", gamma_spe),
+           beta = paste0("beta", gamma_spe))
   
   ## Merge param and data
-  data = merge(data, param)
+  data = merge(data, param, by = c("Country", "Year"))
   
   # Return
   return(data)
