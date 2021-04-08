99edb256cc0e4e2a59ee748892d533c514fca81a
SummerTold <petit.fabien@outlook.com>
2021-04-08 14:30:37 +0200

    rename final to main

--- _script/init.R
+++ _script/init.R
@@ -6,14 +6,7 @@ loc_summeR = file.path(dirname(getwd()), "summeR")
 source(file.path(loc_summeR, "_script", "packages.R"))
 source(file.path(loc_summeR, "_script", "loc_base.R"))
 # Load functions from summeR
-sapply(list.files(pattern = "[.]R$", path = file.path(loc_summeR, "_function"), 
-                  full.names = TRUE), source)
+sapply(list.files(pattern = "[.]R$", path = file.path(loc_summeR, "_function"), full.names = TRUE), source)
 
 # Systime
 Sys.setlocale("LC_TIME", "English")
-
-#### for values ####
-### FUNCTIONS
-# Empty so far
-### PATHS to DATA
-# Empty so far
